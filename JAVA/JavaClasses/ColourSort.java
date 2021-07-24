e2d0f0d9be9ec50afb658727db020575b9fca9c4
shreyassiddanagoudar <83205706+shreyassiddanagoudar@users.noreply.github.com>
2021-07-24 18:16:32 +0530

    Add files via upload

---
+++ JAVA/JavaClasses/ColourSort.java
@@ -0,0 +1,16 @@
+// import java.util.ArrayList;
+import java.util.*;
+public class ColourSort {
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<String>();
+        list.add("g");
+        list.add("j");
+        list.add("a");
+        list.add("b");
+        Collections.sort(list);
+        System.out.println(list);
+        
+
+        // String ele=list.sort(lis);
+    }
+}
