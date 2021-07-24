433b96e42d74d77dc5c0c6bb6ecd0e340267681b
shreyassiddanagoudar <83205706+shreyassiddanagoudar@users.noreply.github.com>
2021-07-24 18:06:31 +0530

    Add files via upload

---
+++ c/Pointers/StoredAddr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char*argv[]) {
+    int *pc;
+    int *pm;
+    int a;
+    int b;
+    int c;
+    pc=&a;
+    pm=&b;
+    printf("%p of pc-%p %p %p of pm is \n ", &pc,pc,&pm,pm);
+    // printf("%p", *a,*b);
+    *pc=10;
+    printf("%p %p %d\n", &pc,pc,&pc);
+    *pm=20;
+     a=*pc + *pm;
+}
