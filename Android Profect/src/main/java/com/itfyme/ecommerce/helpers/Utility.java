c013697f4c8348e9cdd546f258f446b772b34385
shreyassiddanagoudar <83205706+shreyassiddanagoudar@users.noreply.github.com>
2021-08-28 15:11:41 +0530

    Delete Android Profect/src directory

--- Android Profect/src/main/java/com/itfyme/ecommerce/helpers/Utility.java
+++
@@ -1,7 +0,0 @@
-package com.itfyme.ecommerce.helpers;
-
-public class Utility {
-    public static String sessionKey = "sessionID" ;
-    public static String userkey = "userInfo" ;
-    public static String AppName = "Shopping" ;
-}
--- Android Profect/src/main/java/com/itfyme/ecommerce/interfaces/ResponseHandler.java
+++
@@ -1,7 +0,0 @@
-package com.itfyme.ecommerce.interfaces;
-
-public interface ResponseHandler {
-    public void onSuccess(Object data) ;
-    public void onFail(Object data) ;
-    public void onNoData(Object data) ;
-}
